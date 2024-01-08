13a59f3b81166e087f66eb6b7fba86ae526ffd8d
Srinu363 <geddadasri872@gmail.com>
2024-01-08 20:19:31 +0530

    modified init file

--- init.py
+++ init.py
@@ -2,7 +2,9 @@ class hello:
     def __init__(self,name,roll):
         self.name=name
         self.roll=roll
+        print(name,roll)
     def details(self):
         print("this is my name",self.name,self.roll)
 a=hello("srinu",8)
+a1=hello("srinu",9)
 a.details()
