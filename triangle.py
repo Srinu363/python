c98b242a4c1fdfca39773ae4e961fd3976891e76
Srinu363 <geddadasri872@gmail.com>
2024-01-13 12:44:08 +0530

    file is created

--- triangle.py
+++ triangle.py
@@ -1,4 +1,9 @@
-for i in range(10):
-    for j in range(i):
-        print("*",end=" ")
-    print()
+def print_diamond(rows):
+    for i in range(1, rows + 1, 2):
+        spaces = (rows - i) // 2
+        print(" " * spaces + "*" * i + " " * spaces)
+
+
+
+# Example: To print a diamond with 5 rows
+print_diamond(5)
