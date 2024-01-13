1c79028b1032a928df1ff31ded56f6b8915f3219
Srinu363 <geddadasri872@gmail.com>
2024-01-13 12:50:12 +0530

    updated

--- triangle.py
+++ triangle.py
@@ -1,9 +1,11 @@
 def print_diamond(rows):
     for i in range(1, rows + 1, 2):
         spaces = (rows - i) // 2
-        print(" " * spaces + "*" * i + " " * spaces)
+        print("." * spaces + "*" * i + "." * spaces)
 
-
+    for i in range(rows - 2, 0, -2):
+        spaces = (rows - i) // 2
+        print("." * spaces + "*" * i + "." * spaces)
 
 # Example: To print a diamond with 5 rows
 print_diamond(5)
