f523bcbd9b07a96f4853939b9c5e9be9ea395934
Srinu363 <geddadasri872@gmail.com>
2024-03-13 20:17:08 +0530

    prime_numbercode add

--- prime_number.py
+++ prime_number.py
@@ -1,1 +1,12 @@
-a=5
+a = 2
+
+if a <= 1:
+    print("Enter a number greater than one")
+else:
+    for i in range(2, a):
+        if a % i == 0:
+            print("not prime")
+            break
+            
+    else:
+        print("prime")
