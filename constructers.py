fd8e4916e1eba1acaa9940cb424ed9407e7bd98d
Srinu363 <geddadasri872@gmail.com>
2024-03-23 12:02:00 +0530

    main branch all files are deleted

--- constructers.py
+++
@@ -1,6 +0,0 @@
-class hello:
-    def __init__(self):
-        name="naveen"
-        age=30
-        print(name,age)
-c1=hello()
--- linear_search.py
+++
@@ -1,9 +0,0 @@
-a=[1,5,63,7,8,9,2,5,6,1,]
-b=547
-for i in range(len(a)):
-    if a[i]==b:
-        print("element present at index",a[i])
-        break
-else:
-    print("not found ")
-        
