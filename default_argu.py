80251d1383a92d30a85e89058b800655ceb36f90
Srinu363 <geddadasri872@gmail.com>
2024-03-23 11:23:14 +0530

    files are added

---
+++ default_argu.py
@@ -0,0 +1,5 @@
+def fun(country="norway"):
+    print("my country is "+country)
+fun()
+fun("india")
+fun("russia")
---
+++ kwargs.py
@@ -0,0 +1,5 @@
+# if you dont have how many keyword arrugements are passed the use a **
+
+def fun(**kids):
+    print("the first child is "+kids['kid3'])
+fun(kid1="srinu", kid2="prudhvi", kid3="neeraj")
